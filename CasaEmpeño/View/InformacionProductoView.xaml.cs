590011c09ec01a0c2d4b5d83253c9455e4468cbf
Jaime Robles <robles.felix.jaimegabriel@gmail.com>
2025-04-28 11:45:43 -0600

    Una pantalla mas agregada

---
+++ CasaEmpeño/View/InformacionProductoView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace CasaEmpeño.View {
+    public partial class InformacionProductoView : Window {
+        public InformacionProductoView()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarProducto_Click(object sender, RoutedEventArgs e)
+        {
+            // Código para continuar
+        }
+
+        private void SubirFoto_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Seleccionar foto del producto";
+            openFileDialog.Filter = "Archivos de imagen (*.jpg;*.jpeg;*.png)|*.jpg;*.jpeg;*.png";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ProductoImagen.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+                ProductoImagen.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
